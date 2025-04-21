71774b6fa027cf4c543eea733aee0499ca012c92
tutle3310 <kangjianpeng36@gmail.com>
2025-04-21 15:45:43 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace NotePad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnopen = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.rtbText = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnopen
+            // 
+            this.btnopen.Location = new System.Drawing.Point(32, 22);
+            this.btnopen.Name = "btnopen";
+            this.btnopen.Size = new System.Drawing.Size(102, 46);
+            this.btnopen.TabIndex = 0;
+            this.btnopen.Text = "開啟檔案";
+            this.btnopen.UseVisualStyleBackColor = true;
+            this.btnopen.Click += new System.EventHandler(this.btnopen_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(152, 22);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(106, 46);
+            this.btnsave.TabIndex = 1;
+            this.btnsave.Text = "存檔";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // rtbText
+            // 
+            this.rtbText.Location = new System.Drawing.Point(32, 90);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.Size = new System.Drawing.Size(893, 416);
+            this.rtbText.TabIndex = 2;
+            this.rtbText.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(963, 547);
+            this.Controls.Add(this.rtbText);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btnopen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnopen;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.RichTextBox rtbText;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
